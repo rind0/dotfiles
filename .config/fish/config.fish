10f029cc234ce47783339e29bfa8e4f8522e0743
rind0 <924823-rind0@users.noreply.gitlab.com>
2020-04-25 18:39:46 +0900

    .gitignoreの修正

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+# WLSでWindowsのエクスプローラーで開く
+alias open='explorer.exe'
+alias e='explorer.exe .'
+
+alias home='cd $HOME'
+alias whome='cd /mnt/c/Users/rind0'
+
+# exa
+alias ls='exa -abghHliS'
+alias lst='exa -lT'
+
+# procs
+alias ps='procs --sortd cpu'
+alias pst='procs --tree'
+
+# powerline-rs
+function fish_prompt
+  powerline-rs --shell bare $status
+end
+
+# cd ls
+function cd
+  builtin cd $argv
+    lst
+end
