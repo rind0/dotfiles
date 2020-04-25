d59891fa0a2fa1fe86923ba190bf7ccc8aa890f3
rind0 <924823-rind0@users.noreply.gitlab.com>
2020-04-25 18:23:51 +0900

    gitignoreの追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,8 +5,21 @@ alias e='explorer.exe .'
 alias home='cd $HOME'
 alias whome='cd /mnt/c/Users/rind0'
 
+# exa
+alias ls='exa -abghHliS'
+alias lst='exa -lT'
+
+# procs
+alias ps='procs --sortd cpu'
+alias pst='procs --tree'
+
+# powerline-rs
+function fish_prompt
+  powerline-rs --shell bare $status
+end
+
 # cd ls
 function cd
   builtin cd $argv
-    ls -a
+    lst
 end
