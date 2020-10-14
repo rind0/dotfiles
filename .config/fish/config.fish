c865bf8ccfc90312090e1ca60df2ef232e3be005
rind0 <924823-rind0@users.noreply.gitlab.com>
2020-10-14 14:54:46 +0900

    fishのconfigを編集とtheme-bobthefishの追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,3 @@
-# WLSでWindowsのエクスプローラーで開く
-alias open='explorer.exe'
-alias e='explorer.exe .'
-
-alias home='cd $HOME'
-alias whome='cd /mnt/c/Users/rind0'
-
 # exa
 alias ls='exa -abghHliS'
 alias lst='exa -lT'
@@ -13,15 +6,8 @@ alias lst='exa -lT'
 alias ps='procs --sortd cpu'
 alias pst='procs --tree'
 
-# powerline-rs
-function fish_prompt
-  powerline-rs --shell bare $status
-end
-
 # cd ls
 function cd
   builtin cd $argv
     lst
 end
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
