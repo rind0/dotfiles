5658496b0a50b7765a9d9f15cfc9bbe1d9970c08
rind0 <924823-rind0@users.noreply.gitlab.com>
2022-07-15 23:37:53 +0900

    ファイル名と関数名を変更

--- .config/nvim/lua/plugins/lsp/installer.lua
+++ .config/nvim/lua/plugins/lsp/installer.lua
@@ -2,7 +2,7 @@ local M = {}
 
 local lsp_installer_servers = require 'nvim-lsp-installer.servers'
 
-function M.install(servers, force)
+function M.run(servers, force)
     local uninstalled_servers = vim.tbl_filter(function(server)
         return force or server and not server:is_installed()
     end, vim.tbl_map(function(s)
